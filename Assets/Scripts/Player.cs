f54c67447d390a36cc6fcfaad59e41a5e5afa20d
Anurag Negi <101494736+AnuragNegii@users.noreply.github.com>
2024-02-04 12:27:25 +0530

    Animation
    Added player animations and player animator script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7.0f;
-
+    private bool isWalking = false;
     private void Update() {
         Vector2 inputVector = new Vector2(0, 0);
 
@@ -25,7 +25,12 @@ public class Player : MonoBehaviour
             Vector3 moveDir = new Vector3(inputVector.x, 0,inputVector.y);
             transform.position += moveDir * Time.deltaTime * moveSpeed;
 
+            isWalking = moveDir != Vector3.zero;
             float rotateSpeed = 10.0f;
             transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking(){
+        return isWalking;
     }
 }
