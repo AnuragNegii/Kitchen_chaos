8d052c02cf823ee80aed37774111e91000c3a072
Anu <Negianurag99@gmail.com>
2024-02-05 21:26:50 +0530

    Scriptable Objects
    learned about scriptable objects and made scripts like kitchenobjectSO and kitchenObject scripts and used kitchenObject to return the name of scriptableobject

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour {
+
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+
     public void Interact(){
-        Debug.Log("Interact!");
+        Transform kitchenObjecTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        kitchenObjecTransform.localPosition = Vector3.zero;
+
+        Debug.Log(kitchenObjecTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().name);
     }
 }
---
+++ Assets/Scripts/KitchenObjectSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "KitchenObjectSO")]
+public class KitchenObjectSO : ScriptableObject {
+    
+    public Transform prefab;
+    public Sprite image;
+    public string objectName;
+}
